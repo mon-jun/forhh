df11485c43df7f889e6e22905042447abcfee830
rustam781227 <60222829+rustam781227@users.noreply.github.com>
2020-07-17 00:12:03 +0300

    Update AreaCalculator.cs

--- AreaCalculator.cs
+++ AreaCalculator.cs
@@ -3,33 +3,17 @@ namespace AreaCalculator
 {
     public class AreaCalculator
     {
-        public AreaCalculator(double radius)
-        {
-            if (radius < 0)
-                throw new ArgumentException("Radius can't be less than zero");
-            else
-                CalculateArea(radius);
-        }
-        public AreaCalculator(double firstSideOfTriangle, double secondSideOfTriangle, double thirdSideOfTriangle)
-        {
-            if (firstSideOfTriangle <= secondSideOfTriangle + thirdSideOfTriangle ||
-                secondSideOfTriangle <= firstSideOfTriangle + thirdSideOfTriangle ||
-                thirdSideOfTriangle <= firstSideOfTriangle + secondSideOfTriangle)
-                throw new ArgumentException("Parameters must be side of triangle");
-            else
-            {
-                double[] sidesOfTriangle = new double[] { firstSideOfTriangle, secondSideOfTriangle, thirdSideOfTriangle };
-                Array.Sort(sidesOfTriangle);
-                //Third side can be hypotenuse.
-                CalculateArea(sidesOfTriangle[0], sidesOfTriangle[1], sidesOfTriangle[2]);
-            }
-        }
         /// <summary>
         /// Calculates area of circle by its radius.
         /// </summary>
         /// <param name="radius">radius of circle</param>
         /// <returns>Area of circle</returns>
-        public double CalculateArea(double radius) => Math.PI * radius * radius;
+        public double CalculateArea(double radius)
+        {
+            if (radius < 0)
+                throw new ArgumentException("Radius can't be less than zero");
+            Math.PI * radius * radius;
+        }
         /// <summary>
         /// Calculates area of triangle by its sides.
         /// </summary>
@@ -39,6 +23,16 @@ namespace AreaCalculator
         /// <returns>Area of triangle</returns>
         public double CalculateArea(double firstSideOfTriangle, double secondSideOfTriangle, double thirdSideOfTriangle)
         {
+            if (firstSideOfTriangle <= secondSideOfTriangle + thirdSideOfTriangle ||
+                secondSideOfTriangle <= firstSideOfTriangle + thirdSideOfTriangle ||
+                thirdSideOfTriangle <= firstSideOfTriangle + secondSideOfTriangle)
+                throw new ArgumentException("Parameters must be side of triangle");
+            double[] sidesOfTriangle = new double[] { firstSideOfTriangle, secondSideOfTriangle, thirdSideOfTriangle };
+            Array.Sort(sidesOfTriangle);
+            //Third side can be hypotenuse.
+            firstSideOfTriangle = sidesOfTriangle[0];
+            secondSideOfTriangle = sidesOfTriangle[1];
+            thirdSideOfTriangle = sidesOfTriangle[2];
             if (thirdSideOfTriangle * thirdSideOfTriangle == secondSideOfTriangle * secondSideOfTriangle + firstSideOfTriangle * firstSideOfTriangle)
                 return firstSideOfTriangle * secondSideOfTriangle / 2;
             else
